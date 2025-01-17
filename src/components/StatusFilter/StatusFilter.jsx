c3ecfa049ed27dd8e92b2cdb808ef23cbe0d8451
Dmtro_Tr <Di4gamegem@gmail.com>
2025-01-17 16:16:08 +0200

    status management

--- src/components/StatusFilter/StatusFilter.jsx
+++ src/components/StatusFilter/StatusFilter.jsx
@@ -1,12 +1,36 @@
+import { useDispatch, useSelector } from "react-redux";
 import { Button } from "../Button/Button";
 import css from "./StatusFilter.module.css";
+import { setStatusFilter } from "../../redux/action";
 
 export const StatusFilter = () => {
+  // 3. Отримуємо посилання на функцію відправки екшенів
+  const dispatch = useDispatch();
+  const filter = useSelector((state) => state.filters.status);
+  // 4. Викликаємо фабрику екшену та передаємо значення фільтра
+  // 5. Відправляємо результат - екшен зміни фільтра
+  const handleFilterChange = (filter) => dispatch(setStatusFilter(filter));
+
   return (
     <div className={css.wrapper}>
-      <Button>All</Button>
-      <Button>Active</Button>
-      <Button>Completed</Button>
+      <Button
+        selected={filter === "all"}
+        onClick={() => handleFilterChange("all")}
+      >
+        All
+      </Button>
+      <Button
+        selected={filter === "active"}
+        onClick={() => handleFilterChange("active")}
+      >
+        Active
+      </Button>
+      <Button
+        selected={filter === "completed"}
+        onClick={() => handleFilterChange("completed")}
+      >
+        Completed
+      </Button>
     </div>
   );
 };
--- src/components/TaskForm/TaskForm.jsx
+++ src/components/TaskForm/TaskForm.jsx
@@ -1,10 +1,24 @@
+import { useDispatch } from "react-redux";
 import { Button } from "../Button/Button";
 import css from "./TaskForm.module.css";
+import { addTask } from "../../redux/action";
 
 export const TaskForm = () => {
+  // 3. Отримуємо посилання на функцію відправки екшенів
+  const dispatch = useDispatch();
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const form = event.target;
+    // 4. Викликаємо фабрику екшену та передаємо дані для payload
+    // 5. Відправляємо результат – екшен створення завдання
+    dispatch(
+      addTask({
+        id: crypto.randomUUID(),
+        completed: false,
+        text: form.elements.text.value,
+      })
+    );
     form.reset();
   };
 
